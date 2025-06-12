d98eef49c027968a7e359d8e9fd62a7f24744147
João Victor <joaovictorsalotialves12@gmail.com>
2025-06-12 07:50:41 -0300

    create plugins with fastify

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,12 @@
 import { fastify } from 'fastify'
-import { randomUUID } from 'node:crypto'
 
-import { knex } from './database'
 import { env } from './env'
+
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  // const transaction = await knex('transactions')
-  //   .insert({
-  //     id: randomUUID(),
-  //     title: 'Transação de teste',
-  //     amount: 1000,
-  //   })
-  //   .returning('*')
-
-  const transaction = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transaction
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
